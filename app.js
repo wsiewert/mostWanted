08cd732db5881026a8221066782f980260c2b633
Chi <bechi1994@gmail.com>
2017-06-21 10:44:47 -0500

    pull family functions comments

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ function app(people){
 
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people){
-
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
   if(!person){
@@ -46,7 +45,7 @@ function mainMenu(person, people){
     case "quit":
     return; // stop execution
     default:
-    return mainMenu(person, people); // ask again
+    return; // stop execution
   }
 }
 
