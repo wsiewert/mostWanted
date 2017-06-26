61308b8bbb71d25bd9290a19675f2a6c92608520
Chi <bechi1994@gmail.com>
2017-06-26 07:29:43 -0500

    function for refine info

--- app.js
+++ app.js
@@ -122,12 +122,12 @@ return peopleArray;
 function refineSearch(people,criteriaArray, result){
   // TODO: Asks user to choose another criteria.
 
-  let peopleByCriteria = DisplayPersonByCriteria(getPeopleByCriteria(people));
+  let peopleByCriteria = DisplayPersonByCriteria(getPeopleByCriteria(criteria,people,userInput));
   let promptCriteria = ("Here is the list of people who fit that criteria:" +peopleByCriteria+ "\n"+ "Type the name to find further information." + "\n" +  "Or, type one of the following:"+  " " +criteriaArray+ " "+ "\n"+"to refine your search or 'restart' or 'quit'.");
 prompt (promptCriteria);
 }
 
-function DisplayPersonByCriteria (criteriaArray){
+function DisplayPersonByCriteria (people,criteriaArray){
   let arrayToString = criteriaArray.map(function(personObject){
     return " "+personObject.firstName+ " " +personObject.lastName+"\n";
   }).join("");
